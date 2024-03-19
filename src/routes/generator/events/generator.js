1b8ce1b6895874bf9e73c3d688d156330320870d
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-18 21:15:36 -0400

    Events generator working

--- src/routes/generator/events/generator.js
+++ src/routes/generator/events/generator.js
@@ -57,23 +57,60 @@ export function generate(canvas, assets, options) {
         // x1, y1, x2, y2, rows, cols, gap, margin
         const grid = createGrid(0, HEIGHT * 0.125, WIDTH, HEIGHT, panels.length, 1, HEIGHT / 48, HEIGHT / 48);
 
-        // Loop over panels
+        // Draw background
         for (let i = 0; i < panels.length; i++) {
             const rect = grid[i][0];
             longShadow(temp.ctx, HEIGHT / 100, () => {
                 // Extrude color
                 temp.ctx.fillStyle = "rgb(99, 10, 10)";
-                roundRect(temp.ctx, rect.x, rect.y, rect.w, rect.h, HEIGHT / 48).fill()
+                roundRect(temp.ctx, rect.x, rect.y, rect.w, rect.h, HEIGHT / 48).fill();
             }, false);
             // Top color
             temp.ctx.fillStyle = "rgb(116, 22, 22)";
-            roundRect(temp.ctx, rect.x, rect.y, rect.w, rect.h, HEIGHT / 48).fill()
+            roundRect(temp.ctx, rect.x, rect.y, rect.w, rect.h, HEIGHT / 48).fill();
         }
 
         // Draw temp canvas to main canvas
         ctx.globalAlpha = 0.5;
         ctx.drawImage(temp, 0, 0);
         ctx.globalAlpha = 1;
+
+        // Draw text
+
+        for (let i = 0; i < panels.length; i++) {
+            const panel = panels[i];
+            const rect = grid[i][0];
+            const X_MARGIN = HEIGHT / 36;
+            const Y_CENTER = rect.y + rect.h / 2;
+
+            // Details 1
+            ctx.textAlign = "left";
+            ctx.font = `${(HEIGHT / 18)}px "Montserrat-SemiBold"`;
+            longShadow(ctx, HEIGHT / 100, () => {
+                ctx.fillStyle = SHADOW_COLOR;
+                ctx.fillText(panel.details1 || "", rect.x + X_MARGIN, Y_CENTER - HEIGHT / 24);
+            });
+            ctx.fillStyle = "white";
+            ctx.fillText(panel.details1 || "", rect.x + X_MARGIN, Y_CENTER - HEIGHT / 24);
+
+            // Details 2
+            longShadow(ctx, HEIGHT / 100, () => {
+                ctx.fillStyle = SHADOW_COLOR;
+                ctx.fillText(panel.details2 || "", rect.x + X_MARGIN, Y_CENTER + HEIGHT / 24);
+            });
+            ctx.fillStyle = "white";
+            ctx.fillText(panel.details2 || "", rect.x + X_MARGIN, Y_CENTER + HEIGHT / 24);
+
+            // Title
+            ctx.textAlign = "right";
+            ctx.font = `${(HEIGHT / 12)}px "Montserrat-SemiBold"`;
+            longShadow(ctx, HEIGHT / 100, () => {
+                ctx.fillStyle = SHADOW_COLOR;
+                ctx.fillText(panel.title || "", rect.x + rect.w - X_MARGIN, Y_CENTER);
+            });
+            ctx.fillStyle = "white";
+            ctx.fillText(panel.title || "", rect.x + rect.w - X_MARGIN, Y_CENTER);
+        }
     })();
 
     return canvas;
