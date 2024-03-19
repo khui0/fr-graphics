a578cee136fc6ef426a63713196cf589186b987e
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-19 19:02:18 -0400

    Set base

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ export default {
     })
   },
   paths: {
-    base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    base: process.argv.includes("dev") ? "" : "/fr-graphics",
   },
   preprocess: vitePreprocess(),
 };
