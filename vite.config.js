e7000d98c6e9b888797df9d0f2974eca42261ec4
Kenny <101839505+khui0@users.noreply.github.com>
2024-04-01 16:13:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import { webfontDownload } from 'vite-plugin-webfont-dl';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
         sveltekit(),
-        webfontDownload(),
     ],
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
